75b20399a8964e19dd57591fb80f7b1259d3efef
Pierre Gaimard <pierre@gaimard-family.fr>
2021-12-08 20:06:19 +0100

    Add AppFieldError form component

---
+++ src/components/form/AppFieldError.vue
@@ -0,0 +1,34 @@
+<template>
+  <p
+    v-if="error"
+    :aria-invalid="error ? true : null"
+    :class="elmClass"
+    v-bind="attrs"
+  >
+    {{ error }}
+  </p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    error: {
+      type: String,
+      default: '',
+    },
+    attrs: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    elmClass(): string {
+      return this.attrs !== null
+        ? ''
+        : 'text-center text-white mt-2 p-1 rounded-full bg-pink-600'
+    },
+  },
+})
+</script>
