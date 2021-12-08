ee8d2ec2d9256f1a47e56303b9554b679efbd869
Pierre Gaimard <pierre@gaimard-family.fr>
2021-12-08 20:58:57 +0100

    Add tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
+    },
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      pointerEvents: ['hover', 'focus'],
+      margin: ['hover', 'focus'],
+      transform: ['hover', 'focus'],
+      ringWidth: ['hover', 'active'],
+      ringColor: ['hover', 'active'],
+      borderWidth: ['hover', 'focus'],
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
