f4caf707663505ed5deb2bcc034b35e2e821099e
Pierre Gaimard <pierre@gaimard-family.fr>
2021-12-11 14:36:35 +0100

    Add style to app Base component

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
 </template>
 
 <style lang="scss">
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
