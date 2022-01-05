0d7304a4073365ee6f0926712d763292c86b38d9
Pierre Gaimard <pierre@gaimard-family.fr>
2022-01-05 16:05:06 +0100

    add Client class

--- src/services/api/client/Client.ts
+++ src/services/api/client/Client.ts
@@ -1,4 +1,113 @@
-import { AxiosClientAdapter } from '@/services/api/client/Axios/AxiosClientAdapter'
-import { AxiosInstance } from '@/services/api/client/Axios/AxiosInstance'
+import { ClientInterface } from '@/services/api/client/ClientInterface'
+import { ClientRequestOptionsInterface } from '@/services/api/request/ClientRequestOptionsInterface'
+import { ClientResponseInterface } from '@/services/api/response/ClientResponseInterface'
+import { ClientErrorResponse } from '@/services/api/response/ClientErrorResponse'
+import { ClientUnauthorizedErrorResponse } from '@/services/api/response/ClientUnauthorizedErrorResponse'
+import { ClientErrorInterface } from '@/services/api/error/ClientErrorInterface'
+import { ClientNetworkError } from '@/services/api/error/ClientNetworkError'
 
-export const Client = new AxiosClientAdapter(AxiosInstance)
+export class Client {
+  private readonly adapter: ClientInterface
+
+  constructor(adapter: ClientInterface) {
+    this.adapter = adapter
+  }
+
+  public async get(
+    url: string,
+    options?: ClientRequestOptionsInterface,
+  ): Promise<ClientResponseInterface> {
+    try {
+      return await this.adapter.get(url, options)
+    } catch (error) {
+      if (error instanceof ClientErrorResponse) {
+        return Promise.reject(Client.getResponseError(error))
+      }
+
+      return Promise.reject(Client.getError(error))
+    }
+  }
+
+  public async post(
+    url: string,
+    data?: Record<string, unknown>,
+    options?: ClientRequestOptionsInterface,
+  ): Promise<ClientResponseInterface> {
+    try {
+      return await this.adapter.post(url, data, options)
+    } catch (error) {
+      if (error instanceof ClientErrorResponse) {
+        return Promise.reject(Client.getResponseError(error))
+      }
+
+      return Promise.reject(Client.getError(error))
+    }
+  }
+
+  public async put(
+    url: string,
+    data?: Record<string, unknown>,
+    options?: ClientRequestOptionsInterface,
+  ): Promise<ClientResponseInterface> {
+    try {
+      return await this.adapter.put(url, data, options)
+    } catch (error) {
+      if (error instanceof ClientErrorResponse) {
+        return Promise.reject(Client.getResponseError(error))
+      }
+
+      return Promise.reject(Client.getError(error))
+    }
+  }
+
+  public async patch(
+    url: string,
+    data?: Record<string, unknown>,
+    options?: ClientRequestOptionsInterface,
+  ): Promise<ClientResponseInterface> {
+    try {
+      return await this.adapter.patch(url, data, options)
+    } catch (error) {
+      if (error instanceof ClientErrorResponse) {
+        return Promise.reject(Client.getResponseError(error))
+      }
+
+      return Promise.reject(Client.getError(error))
+    }
+  }
+
+  public async delete(
+    url: string,
+    options?: ClientRequestOptionsInterface,
+  ): Promise<ClientResponseInterface> {
+    try {
+      return await this.adapter.delete(url, options)
+    } catch (error) {
+      if (error instanceof ClientErrorResponse) {
+        return Promise.reject(Client.getResponseError(error))
+      }
+
+      return Promise.reject(Client.getError(error))
+    }
+  }
+
+  public setJwt(token: string): void {
+    this.adapter.setJwt(token)
+  }
+
+  private static getError(error: ClientErrorInterface): ClientErrorInterface {
+    if (error.message === 'Network Error') {
+      return new ClientNetworkError(error.message)
+    }
+
+    return error
+  }
+
+  private static getResponseError(error: ClientResponseInterface): ClientResponseInterface {
+    if (error.status === 401) {
+      return new ClientUnauthorizedErrorResponse(error)
+    }
+
+    return error
+  }
+}
