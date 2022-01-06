7d2401eafe71de144d7ada654f02ddec1a8de554
Pierre Gaimard <pierre@gaimard-family.fr>
2022-01-06 07:32:22 +0100

    Add HeroIcon component

---
+++ src/components/utils/HeroIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <component
+    :is="icon"
+    v-bind="$attrs"
+  />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { GiftIcon, DesktopComputerIcon } from '@heroicons/vue/outline'
+
+export default defineComponent({
+  components: {
+    GiftIcon,
+    DesktopComputerIcon,
+  },
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
