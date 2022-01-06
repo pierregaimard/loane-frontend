372b9df4902cccda1f2c085eb966ff74a79399ae
Pierre Gaimard <pierre@gaimard-family.fr>
2022-01-06 07:32:50 +0100

    refactor AppSelect component

--- src/components/form/AppSelect.vue
+++ src/components/form/AppSelect.vue
@@ -1,27 +1,38 @@
 <template>
-  <label :for="uuid">{{ label }}</label>
-  <select
-    :id="uuid"
-    v-model="value"
-    class="app-field"
-    v-bind="$attrs"
-    :aria-describedby="error ? `${uuid}-error` : null"
-    :aria-invalid="error ? true : null"
+  <component
+    :is="inline ? 'span' : 'div'"
+    v-bind="blockAttrs"
   >
-    <option
-      v-for="option in options"
-      :key="option.value"
+    <label
+      :for="uuid"
+      v-bind="labelAttrs"
+      :class="inline ? null : 'block mb-1'"
     >
-      {{ option.label }}
-    </option>
-  </select>
-  <p
-    v-if="error"
-    :id="`${uuid}-error`"
-    class="app-field-error"
-  >
-    {{ error }}
-  </p>
+      {{ label }}
+    </label>
+    <select
+      :id="uuid"
+      v-model="value"
+      class="app-field block"
+      v-bind="$attrs"
+      :aria-describedby="error ? `${uuid}-error` : null"
+      :aria-invalid="error ? true : null"
+      :class="inputStyle"
+    >
+      <option
+        v-for="option in options"
+        :key="option.value"
+        :value="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+    <AppFieldError
+      :id="errorId"
+      :error="error"
+      :attr="errorAttrs"
+    />
+  </component>
 </template>
 
 <script lang="ts">
@@ -46,6 +57,22 @@ export default defineComponent({
       type: String,
       default: '',
     },
+    labelAttrs: {
+      type: Object,
+      default: null,
+    },
+    errorAttrs: {
+      type: Object,
+      default: null,
+    },
+    inline: {
+      type: Boolean,
+      default: false,
+    },
+    blockAttrs: {
+      type: Object,
+      default: null,
+    },
   },
   emits: { 'update:modelValue': null },
   setup() {
@@ -53,6 +80,13 @@ export default defineComponent({
 
     return {
       uuid,
+    }
+  },
+  data: function () {
+    return {
+      errorMessageStyle: {
+        'text-red-600': true,
+      },
     }
   },
   computed: {
@@ -63,6 +97,20 @@ export default defineComponent({
       set(value: [string, number]) {
         this.$emit('update:modelValue', value)
       },
+    },
+    errorId() {
+      return `${this.uuid}-error`
+    },
+    inputStyle() {
+      const baseStyle = 'py-2 px-4 rounded-xl focus:ring-4 focus:shadow-lg'
+      const errorStyle =
+        'text-red-700 placeholder-red-500 border border-red-500 focus:border-red-500 focus:ring-red-300'
+      const validStyle =
+        'text-pink-700 placeholder-pink-300 border-none focus:ring-pink-100'
+
+      return this.error
+        ? `${baseStyle} ${errorStyle}`
+        : `${baseStyle} ${validStyle}`
     },
   },
 })
